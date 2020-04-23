77031094442889435a7ba4a18fe8957ff570a374
hellosophiee <glitchfishzero@gmail.com>
2020-04-23 16:44:57 +0500

    feat: create actions

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,10 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export class Page extends React.Component {
+	onBtnClick = (e) => {
+		const year = +e.currentTarget.innerText
+		this.props.setYear(year)
+	}
 	render() {
 		const { year, photos } = this.props
 		return (
 			<div>
+				<div>
+					<button onClick={this.onBtnClick}>2019</button>
+					<button onClick={this.onBtnClick}>2020</button>
+				</div>
 				<p>
 					У тебя {photos.length} фото за {year} год
 				</p>
@@ -17,4 +25,5 @@ export class Page extends React.Component {
 Page.propTypes = {
 	year: PropTypes.number.isRequired,
 	photos: PropTypes.array.isRequired,
+	setYear: PropTypes.func.isRequired, // добавили новое свойство в propTypes
 }
