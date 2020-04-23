5d655e4a212de1fa70d8e73986e0faaed48aea88
hellosophiee <glitchfishzero@gmail.com>
2020-04-23 23:21:08 +0500

    feat: asynchronous actions

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,14 +1,18 @@
-import { SET_YEAR } from '../actions/PageActions'
+import { GET_PHOTOS_REQUEST, GET_PHOTOS_SUCCESS } from '../actions/PageActions'
 
 const initialState = {
 	year: 2020,
 	photos: [],
+	isFetching: false, // изначально статус загрузки - false т.к. он станет true, когда запрос начнет выполнние
 }
 
 export function pageReducer(state = initialState, action) {
 	switch (action.type) {
-		case SET_YEAR: // изменили строку на константу
-			return { ...state, year: action.payload }
+		case GET_PHOTOS_REQUEST:
+			return { ...state, year: action.payload, isFetching: true }
+
+		case GET_PHOTOS_SUCCESS:
+			return { ...state, photos: action.payload, isFetching: false }
 
 		default:
 			return state
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { rootReducer } from '../reducers'
 // import { ping } from './enhancers/ping' // <-- подключаем наш enhancer
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 // удалили "начальное состояние = initial state" т.к. теперь наш редьюсер составной, и нам нужны initialState каждого reducer'а. Это будет сделано автоматически.
-export const store = createStore(rootReducer, applyMiddleware(logger)) // applyMiddleware(ping) <-- добавляем его в цепочку middleware'ов
+export const store = createStore(rootReducer, applyMiddleware(thunk, logger)) // applyMiddleware(ping) <-- добавляем его в цепочку middleware'ов
